3e687401475e3ee53f94e129c91a560c0907ebcb
Szymon Lanc <lancus3@gmail.com>
2019-01-02 22:05:46 +0100

    Add NPM Color package

---
+++ modules/osInfo.js
@@ -0,0 +1,26 @@
+var os = require('os');
+var OStime = require('./OStime');
+var colors = require('colors');
+
+function OSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:'.grey, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.magenta, cpu);
+    console.log('Uptime: ~'.green, (uptime / 60).toFixed(0), 'min');
+    console.log('User name:'.yellow, userInfo.username);
+    console.log('Home dir:'.gray, userInfo.homedir);
+
+    OStime.print(os.uptime());
+}
+
+exports.print = OSinfo;
